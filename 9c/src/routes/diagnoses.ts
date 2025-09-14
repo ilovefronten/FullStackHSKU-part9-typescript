a6e46e8004ebef0a8a14a1b9b1e942216d530001
stm32dev <stm32dev@outlook.com>
2025-09-14 16:58:32 +0800

    Implement diagnosesService to fetch all diagnoses data

--- 9c/src/routes/diagnoses.ts
+++ 9c/src/routes/diagnoses.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-
+import diagnosesService from "../services/diagnosesService"
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-  res.send('Fetching all diaries!');
+  res.send(diagnosesService.getDiagnoses());
 });
 
 export default router;
