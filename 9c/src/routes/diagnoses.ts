a48f22ee4b7c4e9f9f7c05a0c2c51ab0a2cd4281
stm32dev <stm32dev@outlook.com>
2025-09-14 17:05:41 +0800

    Finish exercise 9.10
    Type res parameter in diagnoses route

--- 9c/src/routes/diagnoses.ts
+++ 9c/src/routes/diagnoses.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import diagnosesService from "../services/diagnosesService"
+import { Response } from 'express'
+import { Diagnosis } from '../types';
+
 const router = express.Router();
 
-router.get('/', (_req, res) => {
+router.get('/', (_req, res: Response<Diagnosis[]>) => {
   res.send(diagnosesService.getDiagnoses());
 });
 
